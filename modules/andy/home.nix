79960d856195a71d1a9b8f7d97227d20149b8996
Andy Scott <andy.g.scott@gmail.com>
2023-10-25 13:35:09 -0700

    add a trick for direnv zsh completions

--- modules/andy/home.nix
+++ modules/andy/home.nix
@@ -174,6 +174,35 @@ lib.mkMerge [
       enableZshIntegration = true;
       nix-direnv.enable = true;
     };
+    programs.zsh = lib.mkIf config.programs.lsd.enable {
+      initExtra = ''
+        _completions_hook() {
+          trap -- ''' SIGINT;
+          if (( ''${+DIRENV_FILE} )); then
+            fpath_before=$fpath
+            typeset -xUT XDG_DATA_DIRS xdg_data_dirs
+            for xdg_data_dir in $xdg_data_dirs; do
+                site_functions="$xdg_data_dir"/zsh/site-functions
+                if [ -d "$site_functions" ]; then
+                    fpath+=("$site_functions")
+                fi
+            done
+            if [[ $fpath != $fpath_before ]]; then
+                compinit
+            fi
+          fi
+          trap - SIGINT;
+        }
+        typeset -ag precmd_functions;
+        if [[ -z "''${precmd_functions[(r)_completions_hook]+1}" ]]; then
+          precmd_functions=( ''${precmd_functions[@]} _completions_hook )
+        fi
+        typeset -ag chpwd_functions;
+        if [[ -z "''${chpwd_functions[(r)_completions_hook]+1}" ]]; then
+          chpwd_functions=( ''${chpwd_functions[@]} _completions_hook )
+        fi
+      '';
+    };
   }
   {
     programs.fzf = {
