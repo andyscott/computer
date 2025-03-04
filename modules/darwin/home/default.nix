a43961fee9e8d310ea904d59c83e141153f7b9d9
Andy Scott <andy.g.scott@gmail.com>
2025-03-04 09:51:09 -0800

    use bat for man pager

--- modules/darwin/home/default.nix
+++ modules/darwin/home/default.nix
@@ -74,6 +74,8 @@
             command cat "$@"
           fi
         }
+
+        export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
       '';
 
       shellAliases = {
