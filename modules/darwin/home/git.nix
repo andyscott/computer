8aadba422d158239ed338dd911f6d57aa769bbf6
Andy Scott <andy.g.scott@gmail.com>
2025-03-03 16:00:04 -0800

    fix git-tardis

--- modules/darwin/home/git.nix
+++ modules/darwin/home/git.nix
@@ -4,18 +4,20 @@ let
   # This is a wrapper around the real git to automatically fix references
   # to the main branch by intelligently replacing master with main and main
   # with master.
-  git-plus = pkgs.symlinkJoin {
+  git-plus = with pkgs; symlinkJoin {
     name = "git+";
     paths = [
-      (pkgs.resholve.writeScriptBin "git"
+      (resholve.writeScriptBin "git"
         {
-          inputs = [ pkgs.git ];
+          inputs = [ git ];
+          # Resholve's syntax checker doesn't like extended glob syntax so
+          # we set the interpreter manually.
           interpreter = "none";
           execer = [
-            ''cannot:${pkgs.git}/bin/git''
+            ''cannot:${git}/bin/git''
           ];
         } ''
-        #!${pkgs.bash}/bin/bash
+        #!${bash}/bin/bash
         shopt -s extglob
         if root="$(git rev-parse --show-toplevel 2> /dev/null)"; then
           if [ -f "$root"/.git/refs/heads/main ]; then
@@ -37,16 +39,24 @@ let
         exec git "$@"
       ''
       )
-      pkgs.git
+      git
     ];
   };
 
   # Time traveling helper for finding the right spot in the reflog
-  git-tardis = pkgs.writeShellScriptBin "git-tardis" ''
-    ${pkgs.git}/bin/git --no-pager log --color -g --abbrev-commit --pretty='%C(auto)%h% D %C(blue)%cr%C(reset) %gs (%s)' \
-      | ${pkgs.fzf}/bin/fzf --ansi \
+  git-tardis = with pkgs; resholve.writeScriptBin "git-tardis"
+    {
+      inputs = [ bash coreutils findutils fzf git-plus ];
+      interpreter = "${bash}/bin/bash";
+      execer = [
+        ''cannot:${fzf}/bin/fzf''
+        ''cannot:${git-plus}/bin/git''
+      ];
+    } ''
+    git --no-pager log --color -g --abbrev-commit --pretty='%C(auto)%h% D %C(blue)%cr%C(reset) %gs (%s)' \
+      | fzf --ansi \
       | cut -d " " -f 1 \
-      | xargs -I {} bash -c "( git name-rev --refs 'heads/*' --no-undefined --name-only {} 2>/dev/null || echo {} )" \
+      | xargs -I {} bash -c "git name-rev --refs 'heads/*' --no-undefined --name-only {} 2>/dev/null || echo {}" \
       | xargs git checkout;
   '';
 
@@ -54,7 +64,13 @@ in
 {
   programs.git = {
     enable = true;
-    package = git-plus;
+    package = pkgs.symlinkJoin {
+      name = "all-the-git";
+      paths = [
+        git-plus
+        git-tardis
+      ];
+    };
 
     extraConfig = {
       color = {
