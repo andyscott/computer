2ec052bfe8982b30b2194788e0f0a767634d26bb
Andy Scott <andy.g.scott@gmail.com>
2025-03-06 10:04:37 -0800

    add some meta

--- modules/darwin/google-meet-escape-artist/default.nix
+++ modules/darwin/google-meet-escape-artist/default.nix
@@ -4,6 +4,27 @@ pkgs.stdenv.mkDerivation {
   pname = "google-meet-escape-artist";
   version = "0.0.0";
 
+  meta = {
+    description = "Google Meet is an Excape Artist";
+    longDescription = ''
+      Automatically moves the Google Meet sticky meeting window out of the way of the
+      cursor.
+
+      The window will move out of the way whenever the cursor moves within a fixed
+      threshold. You can hold <ctrl> to disable the behavior; this allows you to easily
+      interact with the window when needed (e.g. close it fully).
+
+      Background info for the uninitiated:
+
+      Google Meet floats a sticky meeting window on your screen when you tab away
+      from the current meeting. This is nice, but also not nice, as the sticky window
+      very often gets in the way of content on your screen if you're on a small monitor
+      (e.g. MacBook screen).
+
+    '';
+    platforms = pkgs.lib.platforms.darwin;
+  };
+
   src = ./.;
 
   buildInputs = with pkgs; [
