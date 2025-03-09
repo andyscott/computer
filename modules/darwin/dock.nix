f04260f57c9ade85ab0bcf23a7979e95dc889202
Andy Scott <andy.g.scott@gmail.com>
2025-03-09 15:02:13 -0700

    update doc

--- modules/darwin/dock.nix
+++ modules/darwin/dock.nix
@@ -1,18 +1,27 @@
-{ user, ... }: {
+{ lib, user, ... }: {
   imports = [
     ./dock-helper
   ];
 
   local = {
     dock.enable = true;
-    dock.entries = [
-      # { path = "/Applications/Arc.app/"; }
-      { path = "/Applications/Spotify.app/"; }
-      {
-        path = "/Users/${user}/Downloads/";
-        section = "others";
-        options = "--sort dateadded --view fan --display stack";
-      }
+    dock.entries = lib.mkMerge [
+      (lib.mkIf (user == "andy") [
+        { path = "/Applications/Arc.app/"; }
+      ])
+      (lib.mkIf (user == "ags") [
+        { path = "/Applications/Google Chrome.app/"; }
+      ])
+      [
+        { path = "/Applications/Spotify.app/"; }
+        {
+          path = "/Users/${user}/Downloads/";
+          section = "others";
+          options = "--sort dateadded --view fan --display stack";
+        }
+      ]
     ];
   };
 }
+
+
