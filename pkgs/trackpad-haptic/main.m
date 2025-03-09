6d2667171633c8c5ce6aa5872be21a1f60fd2783
Andy Scott <andy.g.scott@gmail.com>
2025-03-09 14:50:04 -0700

    reformat with clang-format

--- pkgs/trackpad-haptic/main.m
+++ pkgs/trackpad-haptic/main.m
@@ -17,31 +17,32 @@ limitations under the License.
 #import <AppKit/AppKit.h>
 
 void printUsage() {
-    printf("Usage: haptic -t [generic|alignment|levelChange]\n");
+  printf("Usage: haptic -t [generic|alignment|levelChange]\n");
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3 || strcmp(argv[1], "-t") != 0) {
-        printUsage();
-        return 1;
-    }
+  if (argc != 3 || strcmp(argv[1], "-t") != 0) {
+    printUsage();
+    return 1;
+  }
 
-    NSString *typeArg = [NSString stringWithUTF8String:argv[2]];
-    NSHapticFeedbackPattern pattern;
+  NSString *typeArg = [NSString stringWithUTF8String:argv[2]];
+  NSHapticFeedbackPattern pattern;
 
-    if ([typeArg isEqualToString:@"generic"]) {
-        pattern = NSHapticFeedbackPatternGeneric;
-    } else if ([typeArg isEqualToString:@"alignment"]) {
-        pattern = NSHapticFeedbackPatternAlignment;
-    } else if ([typeArg isEqualToString:@"levelChange"]) {
-        pattern = NSHapticFeedbackPatternLevelChange;
-    } else {
-        printUsage();
-        return 1;
-    }
+  if ([typeArg isEqualToString:@"generic"]) {
+    pattern = NSHapticFeedbackPatternGeneric;
+  } else if ([typeArg isEqualToString:@"alignment"]) {
+    pattern = NSHapticFeedbackPatternAlignment;
+  } else if ([typeArg isEqualToString:@"levelChange"]) {
+    pattern = NSHapticFeedbackPatternLevelChange;
+  } else {
+    printUsage();
+    return 1;
+  }
 
-    NSHapticFeedbackManager *manager = [NSHapticFeedbackManager defaultPerformer];
-    [manager performFeedbackPattern:pattern performanceTime:NSHapticFeedbackPerformanceTimeNow];
+  NSHapticFeedbackManager *manager = [NSHapticFeedbackManager defaultPerformer];
+  [manager performFeedbackPattern:pattern
+                  performanceTime:NSHapticFeedbackPerformanceTimeNow];
 
-    return 0;
+  return 0;
 }
